20da133c9b9d923508f69dd806793e6f40383533
siwakotisurya <siwakotisurya@outlook.com>
2015-11-05 11:03:33 +0545

    set create action to save user in users table

--- app/controllers/project_monitor_controller.rb
+++ app/controllers/project_monitor_controller.rb
@@ -1,8 +1,19 @@
 class ProjectMonitorController < ApplicationController
   def index
+  @user = User.new
   end
 
   def create
-    
+  @user = User.new(set_db)
+  if @user.save
+  
+  else
+
   end
+  end
+
+  private def set_db
+    params.require(:user).permit(:full_name, :password, :email)
+  end
+
 end
