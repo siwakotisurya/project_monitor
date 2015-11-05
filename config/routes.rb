fa47fabf74b60a5a759923539d84e1e5b97beded
siwakotisurya <siwakotisurya@outlook.com>
2015-11-05 10:45:52 +0545

    root path set as project_monitor#index in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'project_monitor/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'project_monitor#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
